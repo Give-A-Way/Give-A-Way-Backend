7b7dbf409f018ad98b2911df822dd016fa62ae88
farouk0197 <fkatakpaou@gmail.com>
2022-12-02 11:18:51 -0500

    change status functionality completed

--- app.js
+++ app.js
@@ -5,18 +5,38 @@ const app = express();
 const port = 3000; 
 const cors = require('cors');
 const authenRouter = require('./routes/authenticationRouter.js');
-const scheduleDropOff = require("./routes/scheduleDonationRouter.js")
+const donationRouter = require("./routes/scheduleDonationRouter.js");
+const pool = require('./db.js');
 // middleware
 app.use(express.json());
 app.use(cors());
 app.use('/authentication', authenRouter);
-// app.use('/dropoff', scheduleDropOff);
+app.use('/listings', donationRouter);
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/listings', async (req, res) => {
+    const listings = pool.query("SELECT * FROM public.church_listings").then(result=> {return result.rows});
+    console.log(listings)
+  });
+  
 app.listen(port, () =>
   console.log(`Example app listening on port 3000!`),
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
