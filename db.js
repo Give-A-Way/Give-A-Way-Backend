219522201a4b00149464ce9eb027de0363e67c8f
AGarciaNY <aldairgarcia360@gmail.com>
2025-07-02 19:28:08 -0400

    console

--- db.js
+++ db.js
@@ -5,7 +5,6 @@ const env = process.env.NODE_ENV || 'development';
 // Grab the corresponding knex configuration object from knexfile.js
 const knexConfig = require('./knexfile.js')[env];
 // Create the knex connection object using that config
-console.log(knexConfig)
 const knex = require('knex')(knexConfig);
 
 module.exports = knex;
